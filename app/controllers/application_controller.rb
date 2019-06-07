976ff3f0fb09f6fc9dc7ec74a1a8429e1f5d4c67
Ggs91 <georges_atalla@hotmail.fr>
2019-06-07 22:33:38 +0200

    adding date_of_birth to permitted devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :age, :description, :email, :password, :password_confirmation, :department_id, :phone_number, :profil_picture)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :age, :description, :email, :password, :password_confirmation,:department_id, :phone_number, :profil_picture)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :date_of_birth, :description, :email, :password, :password_confirmation, :department_id, :phone_number, :profile_picture)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :date_of_birth, :description, :email, :password, :password_confirmation,:department_id, :phone_number, :profile_picture)}
   end
 end
