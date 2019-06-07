a88540ec3387ecba0a2b87d24e167c7252e020fa
banotraore <47910631+banotraore@users.noreply.github.com>
2019-06-07 15:59:47 +0200

    adding index:true to the comments table for the user and the event

--- db/migrate/20190607102014_create_comments.rb
+++ db/migrate/20190607102014_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :user, foreign_key: true
-      t.references :event, foreign_key: true
+      t.references :user, foreign_key: true, index: true
+      t.references :event, foreign_key: true, index: true
 
       t.timestamps
     end
