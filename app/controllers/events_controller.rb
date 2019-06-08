f90cbfee57cc3ed6f9eb2e58c5c23198d359a242
Ggs91 <georges_atalla@hotmail.fr>
2019-06-08 16:48:22 +0200

    building event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,31 +1,25 @@
 class EventsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+	before_action :set_event, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@events = Event.all
+		@events = Event.all.order("start_date") # sart_date: :desc ??
 	end
-
-  	def show
-			@event = Event.find(params[:id])
-			@comment = Comment.new
-			@comments = @event.comments.order("created_at DESC") #comments will appear on descending order (the last comment will be the first etc..)
-  	end
 
 	def new
 		@event = Event.new
-	end
-
-	def edit
 	end
 
 	def create
 		#@event.category_id = params[:category_id] liste déroulante
 		#@event.department_id = params[:department_id]  liste déroulante
 		@event = Event.new(
-		title: params[:title],
-		description: params[:description],
-		duration: params[:duration],
-		location: params[:location],
-		start_date: params[:start_date])
+			title: params[:title],
+			description: params[:description],
+			duration: params[:duration],
+			location: params[:location],
+			start_date: params[:start_date]
+		)
 		@event.administrator_id = current_user
 		if @event.save
 		flash[:success] = 'Successfully create event !'
@@ -34,6 +28,15 @@ class EventsController < ApplicationController
 		flash[:warning] = 'Oups'
 		redirect_to "/"
 		end
+	end
+
+	def show
+		@event = set_event
+		@comment = Comment.new
+		@comments = @event.comments.order("created_at DESC") #comments will appear on descending order (the last comment will be the first etc..)
+	end
+
+	def edit
 	end
 
 	def update
@@ -50,7 +53,14 @@ class EventsController < ApplicationController
 	end
 
 
-	private
+private
 
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit( :title,  :description, :start_date, :duration, :location, :category, :department)
+	end
 
 end
