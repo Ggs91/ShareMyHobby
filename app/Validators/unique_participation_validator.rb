4753f20674c4068a1d6f5a4f9b945c375174c257
Ggs91 <georges_atalla@hotmail.fr>
2019-06-09 02:10:56 +0200

    creating validator folder

---
+++ app/Validators/unique_participation_validator.rb
@@ -0,0 +1,8 @@
+class UniqueParticipationValidator < ActiveModel::Validator
+  def validate(record)
+  #  if record user
+    #  record.errors[:base] << "User already participate at this event"
+    #end
+  end
+end
+#Make sure a user can't participate twice to the same event
