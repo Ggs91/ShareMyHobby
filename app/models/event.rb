ed19b005ac3eb9619acf668a283f258d51b494c0
Ggs91 <georges_atalla@hotmail.fr>
2019-06-09 19:06:58 +0200

    modifications for datime validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,12 +9,12 @@ class Event < ApplicationRecord
   #Validations
   validate :start_date_cannot_be_in_the_past
   validate :positif_multiple_of_5
-  validates :start_date, presence: {:message => ": Please select a date and time for your event" }
+  validate :date_time_format
   validates :duration, numericality: { only_integer: true }
   validates :title, presence: true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 5..1000 }
   validates :location, presence: true
-  validates :department, presence: {:message => ": Please choose a department" }
+  validates :department, presence: {:message => "must be selected" }
 
   def number_of_places_available
     self.max_participants - self.participants.count
@@ -31,11 +31,14 @@ class Event < ApplicationRecord
 private
 
   def start_date_cannot_be_in_the_past
-     errors.add(:start_date, "can't be in the past") unless start_date.present? && start_date >= DateTime.now
+     errors.add(:start_date, "must be present or can't be in the past") unless start_date.present? && start_date >= DateTime.now
   end
 
   def positif_multiple_of_5
     errors.add(:duration, "must be a multiple of 5") unless duration.present? && duration > 0 && duration % 5 == 0
   end
 
+  def date_time_format
+    errors.add(:date_time, 'must be a valid. Choose a date and a time') if ((DateTime.parse(date_time) rescue ArgumentError) == ArgumentError)
+  end
 end
