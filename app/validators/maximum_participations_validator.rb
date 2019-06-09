c2949f24e64ff4b9afb9d53f2a2b38da6157a5cf
Ggs91 <georges_atalla@hotmail.fr>
2019-06-09 11:13:44 +0200

    creating unique participation validator

--- app/validators/maximum_participations_validator.rb
+++ app/validators/maximum_participations_validator.rb
@@ -1,7 +1,7 @@
 class MaximumParticipationsValidator < ActiveModel::Validator
   def validate(record)
     if record.event.participants.count >= record.event.max_participants
-      record.errors[:base] << "Max participations already reached"
+      record.errors[:base] << "Maximum number of participations already reached"
     end
   end
 end
