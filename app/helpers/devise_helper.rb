22a9ef48abdc883d5018952023c18bf30a0e4214
tsubakiandco <ayuma.royet@gmail.com>
2019-06-10 09:53:10 +0200

    Changing default message error to flash in Devise

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,20 +1,10 @@
 module DeviseHelper
+  
   def devise_error_messages!
-    return '' if resource.errors.empty?
-
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-      <button type="button" class="close" data-dismiss="alert">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <strong>
-       #{pluralize(resource.errors.count, "error")} must be fixed
-      </strong>
-      #{messages}
-    </div>
-    HTML
-
-    html.html_safe
+    if resource.errors.full_messages.any?
+        flash.now[:error] = resource.errors.full_messages
+    end
+    return ''
   end
+
 end
