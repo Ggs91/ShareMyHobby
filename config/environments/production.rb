f46023ec7931e47785398031646189e76531c5bf
banotraore <47910631+banotraore@users.noreply.github.com>
2019-06-10 15:44:27 +0200

    mailjet activated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  #Use mailjet to send the emails from ShareMyHobby (registration, password recovering, etc..)
+  config.action_mailer.delivery_method = :mailjet
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
