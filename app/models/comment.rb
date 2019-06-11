6a6c9865553db25fcc2aff4033830f71f561f611
Ggs91 <georges_atalla@hotmail.fr>
2019-06-11 14:37:54 +0200

    creating instances method for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :event
   validates :body, presence: true, allow_blank: false
 
+  def creation_date
+    self.created_at.strftime("%-d %B %Y - %H:%M")
+  end
+
+  def commenter_full_name
+    self.user.first_name + " " + self.user.last_name
+  end
 end
