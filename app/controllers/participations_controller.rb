6fedb2f397a66bfde3c084d3b3728d02da67c689
Ggs91 <georges_atalla@hotmail.fr>
2019-06-12 22:57:16 +0200

    adding like feature

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -4,25 +4,25 @@ class ParticipationsController < ApplicationController
   before_action :set_event, only: [:index, :create, :destroy]
 
   def index
-    @participations = set_event.participants
+    @participations = @event.participants
   end
 
   def create
-    Participation.create(user: current_user, event: set_event)
+    Participation.create(user: current_user, event: @event)
     flash[:success] = "You're now part of this event !"
-    redirect_to set_event
+    redirect_to @event
   end
 
   def destroy
-    Participation.find_by(user: current_user, event: set_event).destroy
+    Participation.find_by(user: current_user, event: @event).destroy
 		flash[:notice] = "Participation canceled successfully"
-		redirect_to set_event
+		redirect_to @event
   end
 
 private
 
   def set_event
-    Event.find(params.require(:id))
+    @event = Event.find(params.require[:id])
   end
 
   def restrict_access_to_administrator
