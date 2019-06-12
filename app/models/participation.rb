01afe0e8c04d3d0389a9e3bc27c175f3453e20ed
banotraore <47910631+banotraore@users.noreply.github.com>
2019-06-12 12:46:42 +0200

    create the participation email and view

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,6 +1,19 @@
 class Participation < ApplicationRecord
-  validates_with MaximumParticipationsValidator
-  validates_with UniqueParticipationValidator
+
+  #Validations
+  # validates_with MaximumParticipationsValidator
+  # validates_with UniqueParticipationValidator
+
+  #Associations
   belongs_to :user
   belongs_to :event
+
+  #Callbacks
+  after_create :user_participation_mail_send
+
+  private
+
+  def user_participation_mail_send
+    UserMailer.participation_email(self).deliver_now
+  end
 end
