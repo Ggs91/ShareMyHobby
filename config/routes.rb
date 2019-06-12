a83d5e416d42a94d020c226472a2413043848242
banotraore <47910631+banotraore@users.noreply.github.com>
2019-06-12 09:54:45 +0200

    controller picture created to attaach a picture uploaded to an event

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   root 'events#index'
   resources :events, except: [:index] do
     resources :comments, only: [:create]
+    resources :pictures, only: [:create]
   end
 
   devise_for :users do
     resources :profile_pictures, only: [:create]
   end
 
-  #resources :users, only: [:show]
+  resources :users, only: [:show]
   resources :participations, only: [:index, :create, :destroy]
 
   get 'contact', to: 'static_pages#contact'
