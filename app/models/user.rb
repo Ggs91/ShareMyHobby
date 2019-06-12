03bc93abdf0f585add1c72c99b2fd319bb2be202
Ggs91 <georges_atalla@hotmail.fr>
2019-06-12 12:20:42 +0200

    removing phone validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :administrated_events, foreign_key: 'administrator_id', class_name: "Event", dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :attended_events, through: :participations, source: :event, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_events, through: :likes, source: :event, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :profile_picture, dependent: :destroy
   #Validations
@@ -20,7 +22,7 @@ class User < ApplicationRecord
     presence: true,
     uniqueness: true,
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: ": Please enter a valid email format" }  #phone number format also accepts 'N/A', in case it's been set as default value at registration time and not changed when updating
-  validates :phone_number, format: { :allow_blank => true, with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})|N\/A\z/, message: "Please enter a valid number (french format)" }
+#  validates :phone_number, format: { :allow_blank => true, with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})|N\/A\z/, message: "Please enter a valid number (french format)" }
   validates :department, presence: {:message => ": Please choose a department" }
   validates :first_name, presence: true
   validates :last_name, presence: true
