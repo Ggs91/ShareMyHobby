25d71a7d2865561b6fa0bfb96b33fd9a2dbfa451
banotraore <47910631+banotraore@users.noreply.github.com>
2019-06-13 10:57:09 +0200

    create controller friendships and update relation beetwen model user and friendship plus routes and users show view updated

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,8 @@
 class Friendship < ApplicationRecord
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
+  #This is going to be the user_id that we need to follow/unfollow. No relationship will be create without it.
+  validates :follower_id , presence: true
+  validates :followed_id , presence: true
 end
