61dee8bb789ecef95c12ebd2e6ebd4465fee6206
banotraore <47910631+banotraore@users.noreply.github.com>
2019-06-13 10:30:08 +0200

    model Friendship migration

--- db/migrate/20190613082459_create_friendships.rb
+++ db/migrate/20190613082459_create_friendships.rb
@@ -6,5 +6,8 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_index :friendships, :follower_id
+    add_index :friendships, :followed_id
+    add_index :friendships, [:follower_id, :followed_id], unique: true
   end
 end
