03d215269717bdf560f54c6836048335d106b7b5
Ggs91 <georges_atalla@hotmail.fr>
2019-06-13 12:28:05 +0200

    removing unecessary code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,15 @@
 class LikesController < ApplicationController
-  #include LikesHelper
   before_action :set_event
   before_action :set_like, only: [:destroy]
 
   def create
-    if already_liked?
-      flash[:error] = "You can't like twice"
-    else
-      @event.likes.create(user: current_user)
-      redirect_to root_path
-    end
+    @event.likes.create(user: current_user)
+    redirect_to root_path
   end
 
   def destroy
-    if !(already_liked?)
-      flash[:notice] = "Cannot unlike"
-    else
-      @like.destroy
-    end
-  redirect_to root_path
+    @like.destroy
+    redirect_to root_path
   end
 
 private
@@ -29,9 +20,5 @@ private
 
   def set_event
     @event = Event.find(params[:id])
-  end
-
-  def already_liked?
-    Like.where(user: current_user, event: params[:id]).exists?
   end
 end
