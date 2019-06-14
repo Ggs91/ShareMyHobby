71dc6f7a418205488a930ef833f471e2b7c64a1a
tsubakiandco <ayuma.royet@gmail.com>
2019-06-14 21:59:50 +0200

    Update of events and category controller to display pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,12 @@
 class CategoriesController < ApplicationController
+  def index
+  	@categories = Category.all.order("created_at DESC")
+  end
+
   def show
   	@categories = Category.all.order("created_at DESC")
   	@category = Category.find(params[:id])
+  	@events = @category.events
+
   end
 end
